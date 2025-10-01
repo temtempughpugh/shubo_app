ca4fbd0947f8879bc89f318c4c0ac529b71ae70a
SHOGO AIHARA <dbssvvv@MacBook-Air-4.local>
2025-10-01 10:57:40 +0900

    ローカルの変更を保存

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        // TS6133エラーを無視
+        if (warning.code === 'PLUGIN_WARNING') return
+        warn(warning)
+      }
+    }
+  }
 })
